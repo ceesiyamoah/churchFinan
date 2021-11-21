eec07a411d2f846e8cad5d368efa8aa554544df9
ceesiyamoah <ccyamoah@gmail.com>
2021-11-21 19:11:50 +0000

    init

---
+++ screens/Overview.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+const Overview = () => {
+	return (
+		<View style={styles.screen}>
+			<Text>Overview</Text>
+		</View>
+	);
+	P;
+};
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
+export default Overview;
